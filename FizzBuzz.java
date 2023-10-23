842e19102587ca6e7051b76747a97a0a06d4d5d1
Dmytro Karpenko <dmytro.karpenko@mrjaffesclass.com>
2023-10-23 11:29:35 -0700

    final

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -9,16 +9,24 @@ public class FizzBuzz
         int n = 0;
         while(n<100){
             n++;
-            if(n%3 == 0 && n%5==0){
+            int chf = n / 5;
+            int restf = n - 5*chf;
+            int cht = n / 3;
+            int restt = n - 3*cht;
+            
+            if(restf == 0 && restt == 0){
                 System.out.println("FizzBuzz");
             }
-            else if(n%3 == 0){
-                System.out.println("Fizz");
-            }
-            else if(n%5 == 0){
+            else if(restf == 0){
                 System.out.println("Buzz");
             }
-            else System.out.println(n);
+            else if(restt == 0){
+                System.out.println("Fizz");
+            }
+            else{
+                System.out.println(n);
+            }
+            
         }
     }
     public static void main(String[] args){
